f34fcc9b37b1133223b9a42de743a8175a19dc13
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-10-04 11:25:46 +0530

    feature/slider_images
    I added 4 background images for sliders.

--- frontend/src/assets/Components/Common/MainSlider.jsx
+++ frontend/src/assets/Components/Common/MainSlider.jsx
@@ -1,35 +1,59 @@
 import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
-
-import { Pagination, Autoplay  } from 'swiper/modules';
-
+import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css/pagination";
-
-import 'swiper/css/autoplay';
-
-import CookerHood from "./../../Img/CookerHood.jpg"
+import "swiper/css/autoplay";
+import CookerHood from "./../../Img/CookerHood.jpg";
+import WallDecor from "./../../Img/WallDecor.jpg";
+import PantryCupboard from "./../../Img/PantryCupboard.jpg";
+import Sink from "./../../Img/Sink.jpg";
 
 const MainSlider = () => {
+  const BaseSliderStyle = {
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  const Slider1BackgroundStyle = {
+    ...BaseSliderStyle,
+    backgroundImage: `url(${CookerHood})`,
+  };
+
+  const Slider2BackgroundStyle = {
+    ...BaseSliderStyle,
+    backgroundImage: `url(${WallDecor})`,
+  };
+
+  const Slider3BackgroundStyle = {
+    ...BaseSliderStyle,
+    backgroundImage: `url(${PantryCupboard})`,
+  };
+
+  const Slider4BackgroundStyle = {
+    ...BaseSliderStyle,
+    backgroundImage: `url(${Sink})`,
+  };
+
   return (
     <div className="MainSlider">
       <Swiper
-       modules={[Pagination,Autoplay]}
+        modules={[Pagination, Autoplay]}
         spaceBetween={0}
         slidesPerView={1}
         pagination={{ clickable: true }}
-        autoplay={{delay: 10000}}
+        autoplay={{ delay: 10000 }}
         loop={true}
       >
-        <SwiperSlide>
-          <img width="100%" height="100%" src={CookerHood} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide style={Slider1BackgroundStyle}></SwiperSlide>
+        <SwiperSlide style={Slider2BackgroundStyle}></SwiperSlide>
+        <SwiperSlide style={Slider3BackgroundStyle}></SwiperSlide>
+        <SwiperSlide style={Slider4BackgroundStyle}></SwiperSlide>
         ...
       </Swiper>
     </div>
