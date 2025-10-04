130ccfe94952dd46d9dc4716d1e4f17927e4c8eb
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-10-04 23:01:27 +0530

    feature/category-section
    I created category section.

--- frontend/src/assets/Components/Home.jsx
+++ frontend/src/assets/Components/Home.jsx
@@ -2,8 +2,48 @@ import "../css/Style.scss";
 import React from "react";
 import Navigation from "./Common/Navigation";
 import MainSlider from "./Common/MainSlider";
+import Category1 from "./../Img/Category1.jpg";
+import WallDecor from "./../Img/WallDecor.jpg";
+import PantryCupboard from "./../Img/PantryCupboard.jpg";
+import Sink from "./../Img/Sink.jpg";
 
 function Home() {
+  const CategoryBoxContainerStyle = {
+    display: "flex",
+    gap: "50px",
+    width: "auto",
+  };
+
+  const CategoryBoxStyle = {
+    width: "170px",
+    aspectRatio: "1/1",
+    backgroundColor: "red",
+    borderRadius: "50%",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
+  const CategoryBox1Style = {
+    ...CategoryBoxStyle,
+    backgroundImage: `url(${Category1})`,
+  };
+
+  const CategoryBox2Style = {
+    ...CategoryBoxStyle,
+    backgroundImage: `url(${WallDecor})`,
+  };
+
+  const CategoryBox3Style = {
+    ...CategoryBoxStyle,
+    backgroundImage: `url(${PantryCupboard})`,
+  };
+
+  const CategoryBox4Style = {
+    ...CategoryBoxStyle,
+    backgroundImage: `url(${Sink})`,
+  };
+
   return (
     <>
       <header className="shadow">
@@ -17,6 +57,28 @@ function Home() {
       </header>
       {/* Slider Here */}
       <MainSlider />
+      {/* Categories Section */}
+      <div className="container py-5 d-flex align-items-center justify-content-center">
+        <div style={CategoryBoxContainerStyle}>
+          <div className="text-center">
+            <div style={CategoryBox1Style}></div>
+            Cooker Hood
+          </div>
+          <div className="text-center">
+            <div style={CategoryBox2Style}></div>
+            Wall Decor & Panels
+          </div>
+          <div className="text-center">
+            <div style={CategoryBox3Style}></div>
+            Pantry Cupboards
+          </div>
+          <div className="text-center">
+            <div style={CategoryBox4Style}></div>
+            Sinks & Faucets
+          </div>
+        </div>
+      </div>
+      {/* Categories Section End */}
     </>
   );
 }
