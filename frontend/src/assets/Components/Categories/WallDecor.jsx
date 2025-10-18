19c1ddbc26273afa1603e0568ee8c5c7e9d33ecd
Dineth Udantha <163890430+dinethudantha@users.noreply.github.com>
2025-10-18 23:59:56 +0530

    Update WallDecor.jsx

--- frontend/src/assets/Components/Categories/WallDecor.jsx
+++ frontend/src/assets/Components/Categories/WallDecor.jsx
@@ -10,7 +10,7 @@ function WallDecor() {
       <div className="container mt-5">
         <BreadCrumb />
         <SectionHeader
-          title="Wall Decor & Sheets"
+          title="Wall Decor & Panels"
           description="Most Loved by Customers — A Must Have In a High Quality Kitchen"
         />
         <ProductPage />
